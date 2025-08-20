05d0b35cb905e240b708312afcb88190e45421c3
Pratham Handa <prathamhanda10@gmail.com>
2025-08-20 22:36:05 +0530

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <div className="flex justify-center items-center min-h-screen bg-[#EAF3FF]">
+      <div className="text-black text-5xl font-sans">Fooddle Jal 💦</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
